e3cf602d392e1867dde8fd5f7f0d5de5fd0d79a0
Abanoub-Louka <abanoub.louka94@gmail.com>
2024-02-27 21:10:56 +0200

    adding css styling, js rules

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let develpoerName = 'abanoub louka',
+    jobTitle = 'javascript developer',
+    followersNumber = 12.8 + 'k';
+
+    document.querySelector('.name').textContent = develpoerName;
+    document.querySelector('.job-title').textContent = jobTitle;
+    document.querySelector('.stats .stat span.numbers').textContent = followersNumber;
+    document.querySelector('img').setAttribute('src','images/Screenshot 2024-02-23 103757.png');
